ba850bb2850281225637a32f6f055cc4fb28f523
Subham9436 <subham6531@gmail.com>
2025-10-23 20:03:57 +0530

    booking-form-updated

--- frontend/src/components/bookingform.tsx
+++ frontend/src/components/bookingform.tsx
@@ -4,7 +4,7 @@ import { airports } from "../constants/constantValues";
 export default function BookingForm() {
   const [tripType, setTripType] = useState("airport-pickup");
   const [dropType, setDropType] = useState("local-drop");
-
+  const [customerRequirement, setCustomerRequirement] = useState("customer-requirement");
   return (
     <div className="border border-gray-300  rounded-lg shadow-md overflow-hidden mb-6 p-2">
       <div className="w-full max-w-2xl mx-auto">
@@ -56,6 +56,26 @@ export default function BookingForm() {
               </label>
               <div className="flex items-center border rounded px-3 py-2 space-x-2">
                 {/* <MapPin size={18} className="text-gray-600" /> */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                  />
+                </svg>
+
                 <select className="w-full outline-none bg-transparent font-semibold text-gray-800">
                   <option>--Select Airport--</option>
                   {airports.map((airport, index) => (
@@ -71,6 +91,25 @@ export default function BookingForm() {
               </label>
               <div className="flex items-center border rounded px-3 py-2 space-x-2">
                 {/* <MapPin size={18} className="text-gray-600" /> */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                  />
+                </svg>
                 <input
                   type="text"
                   placeholder="Enter Pickup Address"
@@ -130,6 +169,25 @@ export default function BookingForm() {
               </label>
               <div className="flex items-center border rounded px-3 py-2 space-x-2">
                 {/* <MapPin size={18} className="text-gray-600" /> */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                  />
+                </svg>
                 <select className="w-full outline-none bg-transparent font-semibold text-gray-800">
                   <option>--Select Airport--</option>
                   {airports.map((airport, index) => (
@@ -145,6 +203,25 @@ export default function BookingForm() {
               </label>
               <div className="flex items-center border rounded px-3 py-2 space-x-2">
                 {/* <MapPin size={18} className="text-gray-600" /> */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="size-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                  />
+                </svg>
                 <input
                   type="text"
                   placeholder="Enter Pickup Address"
@@ -155,21 +232,64 @@ export default function BookingForm() {
           )}
         </div>
       </div>
-      {tripType === "airport" && <div>
-        <div className="border border-gray-300 rounded-lg mt-4 p-2">
-          <label className="block font-semibold text-gray-700 mb-2">
-            Flight Number <span className="text-red-600">*</span>
-          </label>
-          <div className="flex items-center border rounded px-3 py-2 space-x-2">
-            {/* <MapPin size={18} className="text-gray-600" /> */}
-            <input
-              type="text"
-              placeholder="Enter Pickup Address"
-              className="w-full outline-none bg-transparent font-semibold text-gray-800"
-            />
+      {tripType === "airport" && (
+        <div>
+          <div className="border border-gray-300 rounded-lg mt-4 p-2">
+            <label className="block font-semibold text-gray-700 mb-2">
+              Flight Number <span className="text-red-600">*</span>
+            </label>
+            <div className="flex items-center border rounded px-3 py-2 space-x-2">
+              {/* <MapPin size={18} className="text-gray-600" /> */}
+              <img
+                src="https://bookingform.britwayairporttransfer.co.uk/assets/front/images/flight.svg"
+                className="size-6"
+              ></img>
+              <input
+                type="text"
+                placeholder="Enter Pickup Address"
+                className="w-full outline-none bg-transparent font-semibold text-gray-800"
+              />
+            </div>
           </div>
         </div>
-      </div>}
+      )}
+      {tripType === "airport" && (
+        <div>
+          <div className="border border-gray-300 rounded-lg mt-4 p-2">
+            <label className="font-semibold text-gray-700 mb-2 flex ">
+              PICK UP WAIT TIME{" "}
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="size-4 ml-1"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z"
+                  />
+                </svg>
+              </span>
+            </label>
+            <div className="flex items-center border rounded px-3 py-2 space-x-2">
+              {/* <MapPin size={18} className="text-gray-600" /> */}
+              <img
+                src="https://bookingform.britwayairporttransfer.co.uk/assets/front/images/carbon_time.svg"
+                className="size-6"
+              ></img>
+              <input
+                type="text"
+                placeholder="Enter Pickup Address"
+                className="w-full outline-none bg-transparent font-semibold text-gray-800"
+              />
+            </div>
+          </div>
+        </div>
+      )}
       <div className="mt-4 p-2 border border-gray-300 rounded-lg text-center text-red-700 text-sm font-semibold">
         <h3>+ Customer Requirement</h3>
       </div>
