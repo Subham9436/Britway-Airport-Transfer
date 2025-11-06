aa9fd923452a6c4ba062d24d4c44e0d6a7ec8dc1
Subham9436 <subham6531@gmail.com>
2025-11-06 22:38:44 +0530

    responsiveness-update

--- frontend/src/pages/land.tsx
+++ frontend/src/pages/land.tsx
@@ -7,20 +7,32 @@ export function Land() {
   return (
     <div>
       <Navbar />
-      <div className="bg-[url(/img/banner.png)] bg-cover bg-black/60 bg-blend-overlay p-32 ">
-        <h3 className="font-semibold text-white flex-center text-2xl ">
+      <div
+        className="
+  bg-[url(/img/banner.png)] 
+  bg-cover bg-black/60 bg-blend-overlay 
+  p-10 sm:p-20 md:p-32 
+  text-center
+"
+      >
+        <h3 className="font-semibold text-white text-lg sm:text-xl md:text-2xl">
           Airport Transfers UK | Pickup and Drop-off
         </h3>
-        <div className="flex space-x-2 flex-center mt-2">
-          <h3 className="text-red-700 font-semibold text-3xl">Britway</h3>
-          <h3 className="text-white font-semibold text-3xl">
+
+        <div className="flex justify-center items-center space-x-2 mt-2">
+          <h3 className="text-red-700 font-semibold text-xl sm:text-2xl md:text-3xl">
+            Britway
+          </h3>
+          <h3 className="text-white font-semibold text-xl sm:text-2xl md:text-3xl">
             Airport Transfer
           </h3>
         </div>
-        <h3 className="text-white flex-center mt-2 text-2xl">
+
+        <h3 className="text-white mt-2 text-lg sm:text-xl md:text-2xl">
           Comfortable Pre-booked Vehicle Service
         </h3>
-        <Reviews/>
+
+        <Reviews />
       </div>
       <div className="relative">
         <Card />
